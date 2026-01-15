ce2949c60df89fafcc7e9913d99ef2d33d69282a
Marcelo Alves <alvesmarcelocf@proton.me>
2026-01-15 17:58:35 -0300

    fix: ibus inputMethod isn't necessary on plasma

--- nixos/modules/system.nix
+++ nixos/modules/system.nix
@@ -18,10 +18,6 @@
       LC_TELEPHONE = "en_US.UTF-8";
       LC_TIME = "en_US.UTF-8";
     };
-    inputMethod = {
-      enable = true;
-      type = "ibus";
-    };
   };
 
   # Configure console keymap
