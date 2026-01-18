7ed9d7033114f9c7632bb2b01eaa91ccec5f496a
Marcelo Alves <alvesmarcelocf@proton.me>
2026-01-18 08:21:44 -0300

    feat: add openrgb

---
+++ nixos/modules/openrgb.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+
+{
+  services = {
+    hardware = {
+      openrgb = {
+        enable = true;
+        package = pkgs.openrgb-with-all-plugins;
+        motherboard = "amd";
+        server = {
+          autoStart = false;
+        };
+      };
+    };
+  };
+}
