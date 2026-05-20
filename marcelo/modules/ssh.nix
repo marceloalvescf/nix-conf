bb2c4488980854eb1d33ba0cb3ab28551ef8f76d
Marcelo Alves <alvesmarcelocf@proton.me>
2026-05-20 10:29:29 -0300

    fix(ssh): rename deprecated "matchBlocks" option to "settings"

--- marcelo/modules/ssh.nix
+++ marcelo/modules/ssh.nix
@@ -4,7 +4,7 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks = {
+    settings = {
       "*" = {
         userKnownHostsFile = "~/.ssh/known_hosts";
         identitiesOnly = true;
