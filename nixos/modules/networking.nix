92ecfe2e4d22db396fbc3215333830ecbd3264dd
Marcelo Alves <alvesmarcelocf@proton.me>
2026-05-28 18:19:32 -0300

    chore(networking): add static hosts

--- nixos/modules/networking.nix
+++ nixos/modules/networking.nix
@@ -8,5 +8,14 @@
     networkmanager = {
       enable = true;
     };
+
+    # Add static hosts to /etc/hosts file
+    hosts = {
+      "172.19.0.10" = [
+        "grafana-kind.mapeus.xyz"
+        "prometheus-kind.mapeus.xyz"
+        "kiali-kind.mapeus.xyz"
+      ];
+    };
   };
 }
